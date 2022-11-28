51d756407d38aab4797b912a562de63f9ae9bac5
jamal ahmad <jamalcs007@gmail.com>
2022-11-28 20:40:34 +0500

    changes done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { Stack } from "@mui/material";
-import logo from "../assets/images/logo.png";
+import logo from "../assets/images/Logo.png";
 
 const Navbar = () => {
   return (
